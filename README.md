a4ee93df3fd1c08bfa1362e403bd94ac7d3576f7
liux1299 <liux1299@umn.edu>
2016-11-13 21:36:46 -0600

    Updates to script and README

--- README.md
+++ README.md
@@ -7,7 +7,11 @@ This repository is a collection of generalized scripts that perform various smal
 ### Contains the following scripts:
 
 - `bed_file_summary.R`: provides mean, five number summary and total length of capture region size of a BED file.
- 
+
+- `HarvEST_to_BED.R`: takes a HarvEST file that has PhysPos and Chr_2016 (see `SNP_PhysPosition_Matching.R` script for merging by PhysPos) info based on the new barley reference genome and creates a BED file with 4th column containing SNP ID. The output file was intented to be passed to [`bedops`](http://bedops.readthedocs.io/en/v2p4p21/index.html).
+
 - `random_sampler.R`: randomly samples names/values from a given list
+
+- `SNP_PhysPosition_Matching.R`: tales VCF file (containing physical position, SNP ID, and Chr info) and HarvEST file and adds Physical Position and Chr_2016 columns based on matching SNP names. Note: this script discards any SNPs that do not have matching SNP names.
 
 - `transpose_data.R`: transposes data frames
--- SNP_PhysPosition_Matching.R
+++ SNP_PhysPosition_Matching.R
@@ -1,62 +1,64 @@
-#   This script adds an additional physical positions column to a given file. Physical positions are added based on matching SNP names between two files.
+#!/usr/bin/env Rscript
+
+#   This script adds additional columns to HarvEST SNP_BAC.txt with physical positions and new Chr info for each SNP.
 #   Takes three arguments:
-#   1) positions_file.txt: includes SNP names and Physical positions columns
-#   2) snp_info.txt: new column of physical positions will be added to this file (i.e. HarvEST SNP_BAC.txt)
-#   3) Output filename
+    #   1) VCF file
+    #   2) SNP_BAC.txt file from HarvEST that includes genetic map/Golden Path info
+    #   3) Output filename
 #   Script written by Chaochih Liu
 #   July 21, 2016
 
-#   To run the script: Rscript ./SNP_PhysPosition_Matching.R <positions_file.txt> <snp_info.txt> <output_filename>
-
-#   NOTE: all column names must use underscore NOT a space!
+#   To run the script: Rscript ./SNP_Position_Matching.R <positions_file> <SNP_BAC.txt> <output_filename>
 
 
 #   Take command line arguments
 #   Stores arguments into a vector
 args <- commandArgs(trailingOnly = TRUE)
 
-#   A function to read in positions_file.txt format
-readFilePos <- function(filename) {
+#   A function to read in VCF file
+readVcf <- function(filename) {
     data.file <- read.table(
         file = filename, # passed as an argument
-        header = TRUE, # First line is a header
+        header = FALSE, # First line is a header
         fill = TRUE, # Fill empty fields with NAs
         na.strings = "NA"
     )
-    return(data.file)
+    #   For the merge we are only interested in
+    #   Chr, PhysPos, and SNP_id info
+    vcf.subset <- data.frame(Chr_2016 = data.file$V1, 
+                             PhysPos = data.file$V2, 
+                             SNP_id = data.file$V3)
+    return(vcf.subset)
 }
 
-#   A function to read in snp_info.txt file format
-readFileSnpInfo <- function(filename) {
+#   A function to read in SNP_BAC.txt file format
+readSnpBAC <- function(filename) {
     data.file <- read.delim(
         file = filename, # passed as an argument
         header = TRUE, # First line is a header
         fill = TRUE, # Fill empty fields with NAs
         na.strings = "NA"
     )
+    colnames(data.file) <- c("Query_SNP", "SNP_Found", "BAC", "Seq_Source", "Node", "UCR_FPC_Contig", "Arm", "IBSC_2012_FPC_Contig", "IBSC_2012_LG", "IBSC_2012_cM", "IBSC_2012_Golden_Path", "2014_LG", "2014_cM", "2011_LG", "2011_cM", "2009_LG", "2009_cM")
     return(data.file)
 }
 
 #   A function to merge files
 #   based on matching SNP names
-#   If SNP names match, add position to new column in snp_info.txt file
-mergeFile <- function(physicalData, snpInfoData) {
+#   If SNP names match, add position to new column in HarvEST file
+mergeFile <- function(physicalData, harvestData) {
     #   Merge harvest and physical based on matches found between Query_SNP and SNP_id columns
-    merged <- merge(x = snpInfoData,
+    merged <- merge(x = harvestData,
                     y = physicalData,
-                    #by.x = "Query_SNP", # Use SNP_name column from snp_info.txt for merge
-                    by.x = "SNP_Name",
+                    by.x = "Query_SNP", # Use Query_SNP column from harvestData for merge
                     by.y = "SNP_id", # USE SNP_id column from physicalData for merge
                     all = FALSE # Rows that do not have a match will not be put into final dataframe
-    )
+                    )
     return(merged)
 }
 
 #   A function to write data to outfile
 writeOutFile <- function(mergedData, outFilename) {
-    #colnames(mergedData) <- c("Query_SNP", "SNP_Found", "BAC", "Seq_Source", "Node", "UCR_FPC_Contig", "Arm", "IBSC_2012_FPC_Contig", "IBSC_2012_LG", "IBSC_2012_cM", "IBSC_2012_Golden_Path", "2014_LG", "2014_cM", "2011_LG", "2011_cM", "2009_LG", "2009_cM", "Phys_Position")
-    #colnames(mergedData) <- c("SNP_Name", "Chr", "cM", "FST", "GenBank_ID", "Gene_Short_Name", "Position", "Silent", "Physical Position")
-    #colnames(mergedData) <- c("SNP_name", "Start", "End")
     write.table(x = mergedData,
                 file = outFilename,
                 quote = FALSE,
@@ -69,12 +71,12 @@ writeOutFile <- function(mergedData, outFilename) {
 
 #   Driver function
 main <- function() {
-    physPositionFile <- args[1] # file with physical positions is first argument
-    snpInfoFile <- args[2] # snp_info.txt file is second argument
+    vcfFile <- args[1] # vcf file
+    snpBAC <- args[2] # HarvEST Barley SNP_BAC.txt file is second arguemnt
     outName <- args[3] # name given by user is third argument
-    physical <- readFilePos(filename = physPositionFile) # read in physical positions
-    snpInfo <- readFileSnpInfo(filename = snpInfoFile) # read in snp_info.txt
-    merged <- mergeFile(physicalData = physical, snpInfoData = snpInfo) # merge physical and snpInfo based on matching SNP names
+    physical <- readVcf(filename = vcfFile) # read in physical positions
+    harvest <- readSnpBAC(filename = snpBAC) # read in SNP_BACs
+    merged <- mergeFile(physicalData = physical, harvestData = harvest) # merge physical and harvest based on matching SNP names
     writeOutFile(mergedData = merged, outFilename = outName) # write merged data to outfile
 }
 

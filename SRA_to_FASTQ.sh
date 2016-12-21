30f784a63d711fba6a91afc1308277e1e98396d3
Chaochih Liu <liux1299@umn.edu>
2016-12-21 16:32:23 -0600

    Updates to fastq-dump script

--- SRA_to_FASTQ.sh
+++ SRA_to_FASTQ.sh
@@ -12,17 +12,6 @@ set -o pipefail
 #   Script written by Chaochih Liu
 #   December 21, 2016
 
-#   Define usage message
-function usage() {
-    echo -e "\
-$0: \n\
-\n\
-This script uses fastq-dump. It takes .sra files and outputs gzipped fastq files. \n\
-" >&2
-exit 1
-}
-
-if [[ $# -lt 3 ]]; then usage; fi
 
 #   Dependencies
 module load sratoolkit
@@ -36,4 +25,4 @@ OUT_DIR='/home/morrellp/liux1299/scratch/WBDC_Inversions_Project/WGS_raw_fastq'
 
 #   Dump fastq files into out directory
 #   The -F option ensures it contains only original sequence name
-parallel --dry-run 'fastq-dump --split-files -F --gzip {} --outdir ${OUT_DIR}' ::: ${SRA_FILES}/B1K_04_12.sra
+parallel 'fastq-dump --split-files -F --gzip {} --outdir ${OUT_DIR}' ::: ${SRA_FILES}/Barke*.sra

66296e7ac6f72d8c7cf8a7e295588dc6ca161b5b
liux1299 <liux1299@umn.edu>
2016-12-22 11:19:28 -0600

    Temporary fix

--- SRA_to_FASTQ.sh
+++ SRA_to_FASTQ.sh
@@ -18,11 +18,14 @@ module load sratoolkit
 module load parallel
 
 #   Where are the .sra files located?
-SRA_FILES='/panfs/roc/scratch/pmorrell/WGS_Barley'
+SRA_FILES=/panfs/roc/scratch/pmorrell/WGS_Barley
+
 #   Where do we want our output to go?
 #   As of right now, this part is hardcoded, will eventually fix
-OUT_DIR='/home/morrellp/liux1299/scratch/WBDC_Inversions_Project/WGS_raw_fastq'
+#OUT_DIR=/home/morrellp/liux1299/scratch/WBDC_Inversions_Project/WGS_raw_fastq
 
 #   Dump fastq files into out directory
 #   The -F option ensures it contains only original sequence name
-parallel 'fastq-dump --split-files -F --gzip {} --outdir ${OUT_DIR}' ::: ${SRA_FILES}/Barke*.sra
+#   The -O option gives it the path to output the files
+#   For now, the path following -O needs to be hardcoded in order for fastq-dump to work (need to fix this)
+parallel 'fastq-dump --split-files -F --gzip {} -O /home/morrellp/liux1299/scratch/WBDC_Inversions_Project/WGS_raw_fastq' ::: ${SRA_FILES}/Barke*.sra

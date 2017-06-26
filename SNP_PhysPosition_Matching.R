4724e843485fc91826cf8e45cbe90b5132422d5b
Chaochih Liu <liux1299@umn.edu>
2017-06-26 15:00:03 -0500

    modified script to handle landrace assoc compiled.5e_4.0.01.v2.csv format

--- SNP_PhysPosition_Matching.R
+++ SNP_PhysPosition_Matching.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
 
-#   This script adds additional columns to HarvEST SNP_BAC.txt with physical positions and new Chr info for each SNP.
+#   This script adds additional columns (physical positions and new Chr info for each SNP) to csv file based on matching SNP name.
 #   Takes three arguments:
     #   1) VCF file
-    #   2) SNP_BAC.txt file from HarvEST that includes genetic map/Golden Path info
+    #   2) csv file from that includes SNP name info
     #   3) Output filename
 #   Script written by Chaochih Liu
 #   July 21, 2016
 
-#   To run the script: Rscript ./SNP_Position_Matching.R <positions_file> <SNP_BAC.txt> <output_filename>
+#   To run the script: ./SNP_Position_Matching.R <positions_file> <SNP_BAC.txt> <output_filename>
 
 
 #   Take command line arguments
@@ -25,58 +25,66 @@ readVcf <- function(filename) {
     )
     #   For the merge we are only interested in
     #   Chr, PhysPos, and SNP_id info
-    vcf.subset <- data.frame(Chr_2016 = data.file$V1, 
-                             PhysPos = data.file$V2, 
+    vcf.subset <- data.frame(Chr_2016 = data.file$V1,
+                             PhysPos_2016 = data.file$V2,
                              SNP_id = data.file$V3)
     return(vcf.subset)
 }
 
 #   A function to read in SNP_BAC.txt file format
-readSnpBAC <- function(filename) {
-    data.file <- read.delim(
+readCsv <- function(filename) {
+    data.file <- read.csv(
         file = filename, # passed as an argument
         header = TRUE, # First line is a header
         fill = TRUE, # Fill empty fields with NAs
         na.strings = "NA"
     )
-    colnames(data.file) <- c("Query_SNP", "SNP_Found", "BAC", "Seq_Source", "Node", "UCR_FPC_Contig", "Arm", "IBSC_2012_FPC_Contig", "IBSC_2012_LG", "IBSC_2012_cM", "IBSC_2012_Golden_Path", "2014_LG", "2014_cM", "2011_LG", "2011_cM", "2009_LG", "2009_cM")
+    return(data.file)
+}
+
+fixNaming <- function(data.file) {
+    #   Add 'X' character at the beginning of each character element
+    #   regex pattern "^" is point before first character
+    data.file$SNP <- sub("X", "", data.file$SNP)
     return(data.file)
 }
 
 #   A function to merge files
 #   based on matching SNP names
 #   If SNP names match, add position to new column in HarvEST file
-mergeFile <- function(physicalData, harvestData) {
+mergeFile <- function(physicalData, mainFile) {
     #   Merge harvest and physical based on matches found between Query_SNP and SNP_id columns
-    merged <- merge(x = harvestData,
+    merged <- merge(x = mainFile,
                     y = physicalData,
-                    by.x = "Query_SNP", # Use Query_SNP column from harvestData for merge
+                    by.x = "SNP", # Use SNP column from csv file for merge
                     by.y = "SNP_id", # USE SNP_id column from physicalData for merge
-                    all = FALSE # Rows that do not have a match will not be put into final dataframe
+                    all.x = TRUE, # Rows that do not have a match will remain in dataframe
+                    all.y = FALSE # Rows that do not have a match with x will not be added to x
                     )
     return(merged)
 }
 
 #   A function to write data to outfile
 writeOutFile <- function(mergedData, outFilename) {
-    write.table(x = mergedData,
-                file = outFilename,
-                quote = FALSE,
-                sep = "\t",
-                eol = "\n",
-                col.names = TRUE,
-                row.names = FALSE,
-                na = "NA")
+    write.csv(
+        x = mergedData,
+        file = outFilename,
+        quote = FALSE,
+        eol = "\n",
+        col.names = TRUE,
+        row.names = FALSE,
+        na = "NA")
 }
 
 #   Driver function
 main <- function() {
     vcfFile <- args[1] # vcf file
-    snpBAC <- args[2] # HarvEST Barley SNP_BAC.txt file is second arguemnt
+    csvFile <- args[2] # CSV file with column of SNP names
     outName <- args[3] # name given by user is third argument
     physical <- readVcf(filename = vcfFile) # read in physical positions
-    harvest <- readSnpBAC(filename = snpBAC) # read in SNP_BACs
-    merged <- mergeFile(physicalData = physical, harvestData = harvest) # merge physical and harvest based on matching SNP names
+    maindata <- readCsv(filename = csvFile) # read in main csv
+    maindata.no.x <- fixNaming(data.file = maindata) # remove 'X' at the beginning of SNP names
+    merged <- mergeFile(physicalData = physical.no.x, mainFile = maindata.no.x) # merge physical and main csv based on matching SNP names
     writeOutFile(mergedData = merged, outFilename = outName) # write merged data to outfile
 }
 

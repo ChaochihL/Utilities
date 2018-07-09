b81e64d139cf6341d1f238052708d83e75c6746f
ChaochihL <liux1299@umn.edu>
2018-07-09 16:32:13 -0400

    remove hardcoded parts and add flags

--- SRA_to_FASTQ.sh
+++ SRA_to_FASTQ.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#PBS -l mem=3gb,nodes=1:ppn=16,walltime=24:00:00
-#PBS -m abe
-#PBS -M liux1299@umn.edu
-#PBS -q lab
-
 set -e
 set -u
 set -o pipefail
@@ -12,20 +7,66 @@ set -o pipefail
 #   Script written by Chaochih Liu
 #   December 21, 2016
 
+USAGE="Usage:
+$0 [Option] [SRA file list] [ -d DIR ] [ -h ]
 
-#   Dependencies
-module load sratoolkit
-module load parallel
+Description:
+    This script uses the SRA Toolkit to convert .sra files to gzipped fastq files in parallel.
+    It takes in a list of full filepaths to the .sra files. If DIR is specified, all output
+    files will be put there (script creates DIR if it doesn't exist). If DIR is not
+    supplied, then the current directory is used.
 
-#   Where are the .sra files located?
-SRA_FILES=/panfs/roc/scratch/pmorrell/WGS_Barley
+Dependencies:
+    1) NCBI SRA Toolkit
+    2) GNU parallel
 
-#   Where do we want our output to go?
-#   As of right now, this part is hardcoded, will eventually fix
-#OUT_DIR=/home/morrellp/liux1299/scratch/WBDC_Inversions_Project/WGS_raw_fastq
+Pass -h to see available options."
 
-#   Dump fastq files into out directory
-#   The -F option ensures it contains only original sequence name
-#   The -O option gives it the path to output the files
-#   For now, the path following -O needs to be hardcoded in order for fastq-dump to work (need to fix this)
-parallel 'fastq-dump --split-files -F --gzip {} -O /home/morrellp/liux1299/scratch/WBDC_Inversions_Project/WGS_raw_fastq' ::: ${SRA_FILES}/Barke*.sra
+HELP="Usage:
+$0 [Option] [SRA file list] [ -d DIR ] [ -h ]
+
+Available options:
+Required:
+    -l      Provided [SRA file list]
+Optional:
+    -d DIR  Output all .SRA files into DIR. Provide full filepath to DIR.
+Switches:
+    -h      Show this message and exit.
+"
+
+#   If there are no arguments passed to the script, drop the usage message and exit
+if [ $# == 0 ]
+    then echo "$USAGE"
+    exit 1
+fi
+
+#   Parse the options
+DIR=$(pwd)
+while [[ $# > 0 ]]
+    do
+        FLAG="$1"
+        case $FLAG in
+            -l)
+            SRA_LIST="$2"
+            shift
+            ;;
+            -d)
+            DIR="$2"
+            shift
+            ;;
+            -h)
+            echo "$HELP"
+            exit 2
+            ;;
+            *)
+            echo "$USAGE"
+            exit 1
+            ;;
+        esac
+    shift
+    done
+
+#   SRA to Fastq
+#       -F ensures it contains only original sequence name
+#       -O is the path to output DIR
+parallel 'fastq-dump --split-files -F --gzip {} -O ${DIR}' :::: ${SRA_LIST}

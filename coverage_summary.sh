5876572c5ec377c557dfcf9544b499aa64ccf4a5
ChaochihL <liux1299@umn.edu>
2019-05-22 16:40:34 -0500

    still need to test and fix parallel command

--- coverage_summary.sh
+++ coverage_summary.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#PBS -l mem=1gb,nodes=1:ppn=4,walltime=2:00:00
-#PBS -m abe
-#PBS -M liux1299@umn.edu
-#PBS -q lab
-
 set -e
 set -u
 set -o pipefail
@@ -23,54 +18,104 @@ where: \n\
 2. [bam_dir] is where our current BAM files are located \n\
 3. [region_file] includes regions we are interested in (i.e. BED file) \n\
 4. [out_dir] is where we want our files to go \n\
+\n\
+Required dependencies:
+1. Bedtools v2.28.0 or greater
+2. datamash v1.1.0 or greater
+3. parallel
     " >&2
 exit 1
 }
 
 if [[ $# -lt 3 ]]; then usage; fi
 
-#   Dependencies
-module load bedtools/2.17.0 # Use this version because higher version produces weird output files
-module load datamash/1.1.0
-module load parallel
+#   User provided input arguments
+#   List of bam files
+BAM_LIST=$1
+#   Exome capture regions we are using to calculate coverage
+REGION_FILE=$2
+#   Where do our output files go?
+OUT_DIR=$3
+#   What is the name of our project?
+PROJECT=$4
+
+# Define array to store list of BAM files
+declare -a BAM_ARRAY=($(cat "${BAM_LIST}"))
+
+# Check if output directories exist, if not make them
+mkdir -p ${OUT_DIR} ${OUT_DIR}/Coverage_Summary ${OUT_DIR}/Coverage_Summary/Histograms
 
 #   Function to calculate coverage
-#   Concept of command lines borrowed from Li Lei (https://github.com/lilei1/Utilites/blob/master/coverage_bam_cmd.txt)
-function calcCoverage() {
+#   Command lines adapted from sequence_handling
+#   (https://github.com/MorrellLAB/sequence_handling/blob/master/Handlers/Coverage_Mapping.sh)
+function Calc_Coverage() {
     local bam_file="$1"
-    local bam_dir="$2"
-    local region_file="$3"
-    local out_dir="$4"
+    local region_file="$2"
+    local out_dir="$3"
+    local project="$4"
     #   Sample name
-    sampleName=`basename "${bam_file}" .bam`
+    sampleName=$(basename "${bam_file}" .bam)
     #   Generate coverage hist
-    bedtools coverage -hist -abam "${bam_dir}/${sampleName}.bam" -b "${region_file}" > ${out_dir}/${sampleName}.coverage.hist.txt
-    #   Count number of lines WITHOUT coverage info
-    grep "all" "${out_dir}/${sampleName}.coverage.hist.txt" | wc -l > ${out_dir}/tmp_${sampleName}_noCov.txt
-    #   Calculate quantiles and mean for hist files
-    head -n -$(cat "${out_dir}/tmp_${sampleName}_noCov.txt") "${out_dir}/${sampleName}.coverage.hist.txt" | \
-        datamash --no-strict min 4 q1 4 median 4 mean 4 q3 4 max 4 > ${out_dir}/${sampleName}_coverage_summary.txt
-    echo ${sampleName}
-    echo "Number of lines without coverage info:" $(cat tmp_${sampleName}_noCov.txt)
-    echo "Min   Q1    Median    Mean    Q3    Max"
-    cat ${out_dir}/${sampleName}_coverage_summary.txt
+    bedtools coverage -hist -abam "${bam_file}.bam" -b "${region_file}" > ${out_dir}/Histograms/${sampleName}.hist
+    #   Pull out histogram summarizing coverage among "all" features in A
+    grep ^all ${out_dir}/Histograms/${sampleName}.hist > ${out_dir}/Histograms/${sampleName}.hist.all.txt
+
+    #   Begin calculating statistics per bp
+    #   The minimum is the coverage on the first line of the "all" fields since they're already sorted
+    min=$(head -n 1 ${out_dir}/Histograms/${sampleName}.hist.all.txt | awk -F "\t" '{ print $2 }')
+    #   The maximum is the coverage on the last line of the "all" fields
+    max=$(tail -n 1 ${out_dir}/Histograms/${sampleName}.hist.all.txt | awk -F "\t" '{ print $2 }')
+    #   The mean is the sum of (each coverage * the percent of the genome at that coverage)
+    mean=$(awk '{ sum += $2*$5 } END { print sum }' ${out_dir}/Histograms/${sampleName}.hist.all.txt)
+    #   The mode is the coverage that has the highest percent of the genome at that coverge (excludes zero coverage)
+    mode=$(tail -n +2 ${out_dir}/Histograms/${sampleName}.hist.all.txt | sort -grk5,5 | head -1 | awk -F "\t" '{ print $2 }')
+
+    #   The quantiles are a bit tricky...
+    #   row_count will count how many rows down the "all" fields we are
+    row_count="0"
+    #   freq_sum will be the sum of the frequency fields (column 5) from row 0 to row_count
+    freq_sum="0"
+
+    #   While freq_sum < 0.25
+    while [ $(echo "if (${freq_sum} < 0.25) 1 else 0" | bc) -eq 1 ]
+    do
+        ((row_count += 1))
+        #   freq is the value of the frequency field (column 5) on the row corresponding to row_count
+        freq=$(head -n ${row_count} ${out_dir}/Histograms/${sampleName}.hist.all.txt | tail -1 | awk -F "\t" '{print $5}')
+        #   Add freq to freq_sum until the while loop exits
+        freq_sum=$(echo "${freq_sum} + ${freq}" | bc -l)
+    done
+
+    #   The first quantile is the coverage on the row at which the cumulative frequency hits 0.25 or greater
+    Q1=$(head -n ${row_count} ${out_dir}/Histograms/${sampleName}.hist.all.txt | tail -1 | awk -F "\t" '{print $2}')
+
+    #   Repeat for Q2 (median)
+    while [ $(echo "if (${freq_sum} < 0.5) 1 else 0" | bc) -eq 1 ]
+    do
+        ((row_count += 1))
+        freq=$(head -n ${row_count} ${out_dir}/Histograms/${sampleName}.hist.all.txt | tail -1 | awk -F "\t" '{print $5}')
+        freq_sum=$(echo "${freq_sum} + ${freq}" | bc -l)
+    done
+
+    Q2=$(head -n ${row_count} ${out_dir}/Histograms/${sampleName}.hist.all.txt | tail -1 | awk -F "\t" '{print $2}')
+
+    #   Repeat for Q3
+    while [ $(echo "if (${freq_sum} < 0.75) 1 else 0" | bc) -eq 1 ]
+    do
+        ((row_count += 1))
+        freq=$(head -n ${row_count} ${out_dir}/Histograms/${sampleName}.hist.all.txt | tail -1 | awk -F "\t" '{print $5}')
+        freq_sum=$(echo "${freq_sum} + ${freq}" | bc -l)
+    done
+
+    Q3=$(grep "all" "${out_dir}/Histograms/${sampleName}.hist" | head -n ${row_count} | tail -1 | awk -F "\t" '{print $2}')
+
+    #   Append the statistics to the summary file
+    echo -e "${sampleName}"'\t'"${min}"'\t'"${Q1}"'\t'"${mode}"'\t'"${Q2}"'\t'"${mean}"'\t'"${Q3}"'\t'"${max}" >> "${out_dir}/${project}_coverage_summary_unfinished.txt"
+    #   Put a call to plotCoverage here
 }
 
 #   Export function
-export -f calcCoverage
+export -f Calc_Coverage
 
-#   Arguments provided by user
-#   List of bam files
-BAM_LIST=$1
-#   Directory our bamfiles are located
-BAM_DIR=$2
-#   Exome capture regions we are using to calculate coverage
-REGION_FILE=$3
-#   Where do our output files go?
-OUT_DIR=$4
-
-#   Array of sample names
-#   This code line borrowed from sequence handling (https://github.com/MorrellLAB/sequence_handling/blob/master/Handlers/Coverage_Mapping.sh)
-SAMPLE_NAMES=($(xargs --arg-file="${BAM_LIST}" -I @ --max-args=1 basename @ .bam))
 #   Calculate coverage
-parallel --jobs 2 --xapply calcCoverage {1} "${BAM_DIR}" "${REGION_FILE}" "${OUT_DIR}" :::: "${BAM_LIST}" ::: "${SAMPLE_NAMES[@]}"
+parallel --jobs 2 --xapply Calc_Coverage {1} "${BAM_DIR}" "${REGION_FILE}" "${OUT_DIR}" :::: "${BAM_LIST}" ::: "${SAMPLE_NAMES[@]}"

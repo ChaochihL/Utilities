eb6c61782d44136eab6ba939e348c2a38a83d094
ChaochihL <liux1299@umn.edu>
2020-11-29 13:10:16 -0600

    update script to be more efficient

--- split_fastq/split_fastq.py
+++ split_fastq/split_fastq.py
@@ -23,7 +23,7 @@ Where:
 import sys
 import os
 import math
-from Bio import SeqIO
+import gzip
 
 if len(sys.argv) < 2:
     print(__doc__)
@@ -56,38 +56,70 @@ def prep_out_fnames(file_prefix, num_parts, num_seqs_per_part):
         starting_num_seqs = curr_num_seqs
         curr_num_seqs = curr_num_seqs + num_seqs_per_part
         out_files.append([curr_filename, starting_num_seqs, curr_num_seqs])
+    # Subtract one from upper bound to make the counting easier
+    for i, elem in enumerate(out_files):
+        out_files[i][2] = out_files[i][2] - 1
     return out_files
 
 
-def split_fastq_file(record_dict, out_files, out_dir):
-    """Split our fastq file into N parts and save to file."""
+def split_fastq_file(f, out_files, out_dir):
+    """Split our fastq file into N parts and save to file. We also minimize
+    the number of times the output part files are opened/closed to reduce
+    overhead. Each part file is only opened once and closed once."""
     # Split file and write to parts
     findex = 0  # Start at file index 0
     curr_count = 0
-    for key in record_dict:
-        print(curr_count)
-        if curr_count >= out_files[findex][1] and curr_count < out_files[findex][2]:
-            print("Current file index is:", findex)
+    # Iterate line by line
+    for i in f:
+        if (curr_count >= out_files[findex][1] and curr_count <= out_files[findex][2]):
+            # print("Current file index is:", findex)
             # Output sequence to current part
             curr_out_fp = out_dir + "/" + out_files[findex][0]
-            print(curr_out_fp)
-            with open(curr_out_fp, "a") as output_handle:
-                SeqIO.write(record_dict[key], output_handle, "fastq")
-            # Increment by one each sequence
-            curr_count += 1
-            print("after writing sequence count:", curr_count)
-        elif curr_count >= out_files[findex][2]:
-            # Move on to next output file
-            findex += 1
-            print("Current file index is:", findex)
+            # Open file (one time) for writing if file doesn't exist
+            if not os.path.exists(curr_out_fp):
+                # Create and open file
+                print("Opening file:", curr_out_fp)
+                cof = open(curr_out_fp, 'a+')
+            # Write line to file
+            cof.write(i)
+            # Increment by one each sequence (starts with "@")
+            if i.startswith("@"):
+                curr_count += 1
+            # print("after writing sequence count:", curr_count)
+        elif curr_count > out_files[findex][2] and not i.startswith("@"):
+            # print("Current file index is:", findex)
+            # Write line to file
+            cof.write(i)
+            # print("after writing sequence count:", curr_count)
+        elif curr_count > out_files[findex][2] and i.startswith("@"):
+            # Check if we are processing our last sequence
+            if findex == len(out_files) - 1:
+                # Don't increment file index anymore
+                print("We are at the last sequence.")
+            else:
+                # Close previous part file before incrementing
+                print("Closing file:", curr_out_fp)
+                cof.close()
+                # Move on to next output file
+                findex += 1
+            # print("Current file index is:", findex)
             # Output sequence to current part
             curr_out_fp = out_dir + "/" + out_files[findex][0]
-            print(curr_out_fp)
-            with open(curr_out_fp, "a") as output_handle:
-                SeqIO.write(record_dict[key], output_handle, "fastq")
-            # Increment by one each sequence
-            curr_count += 1
-            print("after writing sequence count:", curr_count)
+            # print(curr_out_fp)
+            # Open file (one time) for writing
+            if not os.path.exists(curr_out_fp):
+                # Create and open file
+                print("Opening file:", curr_out_fp)
+                cof = open(curr_out_fp, 'a+')
+            # Write line to file
+            cof.write(i)
+            # Increment by one each sequence (starts with "@")
+            if i.startswith("@"):
+                curr_count += 1
+            # print("after writing sequence count:", curr_count)
+    # Finished iterating through all sequences, close last part file
+    print("Closing file:", curr_out_fp)
+    cof.close()
 
 
 def main(fastq_file_path, num_parts, total_num_seqs, out_dir_fp):
@@ -98,8 +130,6 @@ def main(fastq_file_path, num_parts, total_num_seqs, out_dir_fp):
     out_dir = os.path.expanduser(out_dir_fp).rstrip("/")
     # Prepare file prefix for split output files
     file_prefix = prep_file_prefix(fastq_fp)
-    # Load dictionary indices of each sequence
-    record_dict = SeqIO.index(fastq_fp, "fastq")
     # Identify the number of sequences to output in each part
     num_seqs_per_part = math.ceil(int(total_num_seqs) / int(num_parts))
     # Generate list of output filenames
@@ -109,11 +139,19 @@ def main(fastq_file_path, num_parts, total_num_seqs, out_dir_fp):
     for l in out_files:
         curr_out_fp = out_dir + "/" + l[0]
         if os.path.exists(curr_out_fp):
+            print("More than 1 part file exists, remove to start from clean slate:", curr_out_fp)
             os.remove(curr_out_fp)
         else:
             print(l[0], "file doesn't exist, proceeding to split file...")
     # Split fastq file into parts and save to file
-    split_fastq_file(record_dict, out_files, out_dir)
+    # Read in file line by line
+    if "gz" in fastq_fp:
+        with gzip.open(fastq_fp, 'rt') as f:
+            split_fastq_file(f, out_files, out_dir)
+    else:
+        with open(fastq_fp, 'r') as f:
+            split_fastq_file(f, out_files, out_dir)
+    print("Done splitting fastq files.")
 
 
 main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
